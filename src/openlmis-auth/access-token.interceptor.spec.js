f58a3158835d0167bf66e6bf0f66db494fff85c8
Paweł Albecki <palbecki@soldevelo.com>
2017-06-12 19:09:03 +0200

    OLMIS-2596: improvements in acces-token interceptor tests

--- src/openlmis-auth/access-token.interceptor.spec.js
+++ src/openlmis-auth/access-token.interceptor.spec.js
@@ -129,13 +129,12 @@ describe('accessTokenInterceptor', function() {
 
         it('should show error.atuhorization alert on 403 status', function() {
             response.status = 403;
-            response.data = {};
-            response.data.message = 'Test message';
+            response.data = {message: 'Test message'};
 
             provider.responseError(response);
 
             expect(alertMock.error)
-                .toHaveBeenCalledWith('openlmisAuth.authorization.error', 'Test message');
+                .toHaveBeenCalledWith('openlmisAuth.authorization.error', response.data.message);
         });
 
         it('should reject response', function() {
