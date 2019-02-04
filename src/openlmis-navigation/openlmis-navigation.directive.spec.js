4cb2b91921b06df79898fa0961bed3af098b754e
Nikodem Graczewski <ngraczewski@soldevelo.com>
2019-02-04 15:59:11 +0100

    OLMIS-5976: Refactored openlmis-navigation.directive.spec.js

--- src/openlmis-navigation/openlmis-navigation.directive.spec.js
+++ src/openlmis-navigation/openlmis-navigation.directive.spec.js
@@ -14,55 +14,61 @@
  */
 
 describe('openlmis-navigation directive', function() {
-    var element, scope;
 
-    beforeEach(module('openlmis-navigation'));
+    beforeEach(function() {
+        module('openlmis-navigation', function($stateProvider) {
+            $stateProvider.state('test', {
+                showInNavigation: true,
+                label: 'test'
+            });
+            $stateProvider.state('offline', {
+                showInNavigation: true,
+                label: 'offline',
+                isOffline: true
+            });
+            $stateProvider.state('foo', {
+                showInNavigation: true,
+                label: 'foo'
+            });
+            $stateProvider.state('foo.bar', {
+                showInNavigation: true,
+                label: 'bar'
+            });
+            $stateProvider.state('foo.bar.baz', {
+                showInNavigation: true,
+                label: 'baz'
+            });
+            $stateProvider.state('foo.test', {
+                showInNavigation: true
+            });
+        });
 
-    beforeEach(module(function($stateProvider) {
-        $stateProvider.state('test', {
-            showInNavigation: true,
-            label: 'test'
+        inject(function($injector) {
+            this.$rootScope = $injector.get('$rootScope');
+            this.$compile = $injector.get('$compile');
+            this.offlineService = $injector.get('offlineService');
         });
-        $stateProvider.state('offline', {
-            showInNavigation: true,
-            label: 'offline',
-            isOffline: true
-        });
-        $stateProvider.state('foo', {
-            showInNavigation: true,
-            label: 'foo'
-        });
-        $stateProvider.state('foo.bar', {
-            showInNavigation: true,
-            label: 'bar'
-        });
-        $stateProvider.state('foo.bar.baz', {
-            showInNavigation: true,
-            label: 'baz'
-        });
-        $stateProvider.state('foo.test', {
-            showInNavigation: true
-        });
-    }));
+
+        spyOn(this.offlineService, 'isOffline').andReturn(true);
+
+        var markup = '<openlmis-navigation></openlmis-navigation>';
+
+        this.scope = this.$rootScope.$new();
+        this.element = this.$compile(markup)(this.scope);
+
+        this.scope.$apply();
+    });
 
     describe('shows navigation', function() {
-        beforeEach(inject(function($rootScope, $compile) {
-            var markup = '<openlmis-navigation></openlmis-navigation>';
-
-            scope = $rootScope.$new();
-            element = $compile(markup)(scope);
-
-            scope.$apply();
-        }));
 
         it('will display root states', function() {
-            var firstLevel = element.children();
+            var firstLevel = this.element.children();
 
             expect(firstLevel.length).toBe(3);
         });
 
         it('will show child items in a drop down', function() {
-            var dropdown = element.children('.dropdown:first');
+            var dropdown = this.element.children('.dropdown:first');
 
             var childStates = dropdown.children('ul').children();
 
@@ -71,7 +77,7 @@ describe('openlmis-navigation directive', function() {
         });
 
         it('will recurse through child item dropdowns', function() {
-            var dropdown = element.children('.dropdown:first').children('ul')
+            var dropdown = this.element.children('.dropdown:first').children('ul')
                 .children('.dropdown:first');
 
             var childStates = dropdown.children('ul').children();
@@ -83,24 +89,9 @@ describe('openlmis-navigation directive', function() {
     });
 
     describe('offline', function() {
-        var offline = true;
-        beforeEach(inject(function(offlineService) {
-            spyOn(offlineService, 'isOffline').andCallFake(function() {
-                return offline;
-            });
-        }));
-
-        beforeEach(inject(function($rootScope, $compile) {
-            var markup = '<openlmis-navigation></openlmis-navigation>';
-
-            scope = $rootScope.$new();
-            element = $compile(markup)(scope);
-
-            scope.$apply();
-        }));
 
         it('will disable states that dont support offline', function() {
-            var testLink = element.children('li:last').children('a');
+            var testLink = this.element.children('li:last').children('a');
 
             expect(testLink.text()).toBe('test');
             expect(testLink.attr('disabled')).toBe('disabled');
@@ -108,7 +99,7 @@ describe('openlmis-navigation directive', function() {
         });
 
         it('states marked offline will not be disabled', function() {
-            var testLink = angular.element(element.children('li')[1]).children('a');
+            var testLink = angular.element(this.element.children('li')[1]).children('a');
 
             expect(testLink.text()).toBe('offline');
             expect(testLink.attr('disabled')).toBe(undefined);
